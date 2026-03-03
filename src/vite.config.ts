8c06fd5bd332b044ea26955e3363f252881a8e84
Elab'Site <contact@elabsite.fr>
2026-03-03 11:27:10 +0100

    Initial commit

---
+++ src/vite.config.ts
@@ -0,0 +1,19 @@
+// NOTE: configuration en CommonJS pour supporter `__dirname` sans `import.meta`.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+// Pour GitHub Pages, définissez `base` sur le nom du repo, ex: "/mon-repo/"
+// (Aucune référence/dépendance à Lovable)
+
+module.exports = {
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [require("@vitejs/plugin-react-swc")()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+};
