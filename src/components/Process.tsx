c8754857b872c0d841ae6109cf719b66d2ebba63
ElabSite <contact@elabsite.fr>
2026-03-04 23:39:55 +0100

    Change grid layout from 4 to 3 columns

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -34,7 +34,7 @@ const Process = () => {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {steps.map((step, index) => (
             <div 
               key={index}
