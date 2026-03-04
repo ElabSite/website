3b3b3c47a4437b02bc56b717d774dc1a3916cd47
ElabSite <contact@elabsite.fr>
2026-03-04 22:11:29 +0100

    Disable Testimonials component in Index page
    Comment out Testimonials component import and usage.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import Services from "@/components/Services";
 import Process from "@/components/Process";
 import Benefits from "@/components/Benefits";
 import PricingAdvantage from "@/components/PricingAdvantage";
-import Testimonials from "@/components/Testimonials";
+//import Testimonials from "@/components/Testimonials";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
 
@@ -17,7 +17,7 @@ const Index = () => {
       <Process />
       <Benefits />
       <PricingAdvantage />
-      <Testimonials />
+      {/*<Testimonials />*/}
       <CTA />
       <Footer />
     </main>
