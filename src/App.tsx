cee438089f1033c45229f94aeaddf47d5679375b
ElabSite <contact@elabsite.fr>
2026-03-04 14:33:31 +0100

    Remove BrowserRouter and simplify routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Tarifs from "./pages/Tarifs";
 import Realisations from "./pages/Realisations";
@@ -22,21 +22,19 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/tarifs" element={<Tarifs />} />
-          <Route path="/realisations" element={<Realisations />} />
-          <Route path="/creation-site-internet-chambery" element={<CreationSiteChambery />} />
-          <Route path="/creation-site-internet-grenoble" element={<CreationSiteGrenoble />} />
-          <Route path="/creation-site-internet-annecy" element={<CreationSiteAnnecy />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/mentions-legales" element={<MentionsLegales />} />
-          <Route path="/politique-de-confidentialite" element={<PolitiqueConfidentialite />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/tarifs" element={<Tarifs />} />
+        <Route path="/realisations" element={<Realisations />} />
+        <Route path="/creation-site-internet-chambery" element={<CreationSiteChambery />} />
+        <Route path="/creation-site-internet-grenoble" element={<CreationSiteGrenoble />} />
+        <Route path="/creation-site-internet-annecy" element={<CreationSiteAnnecy />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/mentions-legales" element={<MentionsLegales />} />
+        <Route path="/politique-de-confidentialite" element={<PolitiqueConfidentialite />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
